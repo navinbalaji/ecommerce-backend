d280ff00870b595dcd4a8cc4b07ada9ddc099623
navinbalaji <naveenbalaji1199@gmail.com>
2024-09-29 10:52:02 +0530

    limit increased

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use(
     })
 );
 app.use(morgan('tiny'));
-app.use(express.json())
+app.use(express.json({limit: '15mb'}))
 app.get('/ping', (_, res) => res.status(200).send('pong'));
 app.use('/auth', authRouter);
 app.use('/product', productRouter);
